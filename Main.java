6574865d91b253c6b50e8c976ec2a8f43e183cba
Amandacorreia1 <115305715+Amandacorreia1@users.noreply.github.com>
2023-11-21 20:07:00 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package Construtor;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Veiculo v1 = new Veiculo("Fiat", "Uno", 2023);
+		Veiculo v2 = new Veiculo("Honda", "Civic", 2022, 300);
+		
+		v1.imprimirVeiculo();
+		v2.imprimirVeiculo();
+
+
+	}
+
+}
